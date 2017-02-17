2cb61e818c9886d16a61e9de0feffd12d9fa120e
liuyuhong1981 <liuyuhong1981@gmail.com>
2017-02-17 14:59:50 +0800

    update

--- src/main/java/org/lyh/myweb/aspect/ServiceAspect.java
+++ src/main/java/org/lyh/myweb/aspect/ServiceAspect.java
@@ -6,10 +6,15 @@
  */
 package org.lyh.myweb.aspect;
 
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -24,5 +29,30 @@ public class ServiceAspect {
 	public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 		System.out.println("around advice: A request was issued at " + proceedingJoinPoint.toLongString());
 		return proceedingJoinPoint.proceed();
+	}
+
+	@AfterThrowing(pointcut = "execution(* com.gkatzioura.spring.aop.service.*.*(..))", throwing = "error")
+	public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
+		String message = "";
+		if(error.getMessage().contains("my")) {
+			message = "get my exception message and change it !";
+		} else {
+			message = error.getMessage();
+		}
+		Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+		Object[] args = joinPoint.getArgs();
+		String methodName = method.getName();
+		StringBuilder builder = new StringBuilder(512);
+		builder.append("\nMethod Name is :").append(methodName).append("\n");
+		if (args != null) {
+			int size = args.length;
+			builder.append("Method Args:\n");
+			for (int index = 0; index < size; index++) {
+				Object object = args[index];
+				builder.append("args[").append(index).append("]:").append(object.toString()).append("\n");
+			}
+		}
+		builder.append(message);
+		throw new RuntimeException(builder.toString());
 	}
 }
